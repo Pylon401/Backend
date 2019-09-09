5c319ee257b74fbb2e2de49195089d7d9d6cd31e
Skyler Burger <skylerburger@gmail.com>
2019-09-09 13:59:54 -0700

    Adds GitHub API

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ async def fetch(session, url):
 
 # The NORMALIZE Level
 async def normalize_pypi(session, url):
+    print(f'Norm/fetch for {url} started.')
     feed_data = feedparser.parse(await fetch(session, url))
     entries = feed_data.entries
     normalized_entries = []
@@ -28,12 +29,18 @@ async def normalize_pypi(session, url):
             'desc': entry['summary']
         })
 
+    print(f'Norm/fetch for {url} completed.')
     return normalized_entries
 
 async def normalize_github(session, url):
+    print(f'Norm/fetch for {url} started.')
     response = await fetch(session, url)
+    print(f'Norm/fetch for {url} completed.')
+    entries = json.loads(response)
+    entries = entries['items'][:20]
     normalized_entries = []
-    for entry in response:
+    
+    for entry in entries:
         normalized_entries.append({
             'source': 'github',
             'title': entry['name'],
@@ -48,8 +55,11 @@ async def normalize_github(session, url):
 async def main(request):
     entries = []
     async with ClientSession() as session:
-        entries.append(await normalize_pypi(session, 'https://pypi.org/rss/updates.xml'))
-        entries.append(await normalize_pypi(session, 'https://pypi.org/rss/packages.xml'))
+        result_1 = await normalize_pypi(session, 'https://pypi.org/rss/updates.xml')
+        result_2 = await normalize_pypi(session, 'https://pypi.org/rss/packages.xml')
+        result_3 = await normalize_github(session, 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc')
+        result_4 = await normalize_github(session, 'https://api.github.com/search/repositories?q=language:python&sort=updated&order=desc')
+        entries = result_1 + result_2 + result_3 + result_4
 
     return web.Response(text=json.dumps(entries))
 
