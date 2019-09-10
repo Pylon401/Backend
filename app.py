14beb2ab5a39ac1357565b21c3e7d3655124808d
Skyler Burger <skylerburger@gmail.com>
2019-09-10 07:58:48 -0700

    Restore GitHub integration

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ import feedparser
 
 import asyncio
 import json
+import time
 
-# URLS
-# https://pypi.org/rss/updates.xml
-# https://pypi.org/rss/packages.xml
-# https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc
-# https://api.github.com/search/repositories?q=language:python&sort=updated&order=desc
 
-# The FETCH Level
 async def fetch(session, url):
+    """
+    Takes in a ClientSession and a URL string.
+    Awaits a request to the given URL.
+    Returns the payload.
+    """
     async with session.get(url) as response:
         return await response.text()
 
-# The NORMALIZE Level
+
 async def normalize_pypi(session, url):
+    """
+    Takes in a ClientSession and a URL string to PyPI.
+    Awaits a fetch coroutine, then normalizes the payload.
+    Returns the normalized entries.
+    """
     print('url start', url)
     feed_data = feedparser.parse(await fetch(session, url))
     print('url done', url)
@@ -32,46 +37,58 @@ async def normalize_pypi(session, url):
 
     return normalized_entries
 
+
 async def normalize_github(session, url):
+    """
+    Takes in a ClientSession and a URL string to GitHub.
+    Awaits a fetch coroutine, then normalizes the payload.
+    Returns the normalized entries.
+    """
     print('url start', url)
     response = json.loads(await fetch(session, url))
     print('url done', url)
+    entries = response['items']
     normalized_entries = []
 
-    # TODO: Turn response into json before parsing.
-
-    for entry in response:
-        normalized_entries.append({'foo':'bar'})
-        # normalized_entries.append({
-        #     'source': 'github',
-        #     'title': entry['name'],
-        #     'link': entry['html_url'],
-        #     'desc': entry['description'],
-        #     'stars': entry['stargazers_count']
-        # })
+    for entry in entries:
+        normalized_entries.append({
+            'source': 'github',
+            'title': entry['name'],
+            'link': entry['html_url'],
+            'desc': entry['description'],
+            'stars': entry['stargazers_count']
+        })
 
     return normalized_entries
 
 
-# The GATHER Level
 async def main(request):
+    """
+    Takes in a Request object from the client.
+    Creates a ClientSession and coroutines for each API.
+    Awaits the normalized entries from the gathered coroutines.
+    Returns all of the normalized entries.
+    """
+    start_time = time.perf_counter()
     entries = []
     async with ClientSession() as session:
         entries.append(normalize_github(session, 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'))
+        entries.append(normalize_github(session, 'https://api.github.com/search/repositories?q=language:python&sort=updated&order=desc'))
         entries.append(normalize_pypi(session, 'https://pypi.org/rss/updates.xml'))
         entries.append(normalize_pypi(session, 'https://pypi.org/rss/packages.xml'))
-        entries.append(normalize_github(session, 'https://api.github.com/search/repositories?q=language:python&sort=updated&order=desc'))
 
         results = await asyncio.gather(*entries)
 
-
+    elapsed_time = time.perf_counter() - start_time
+    print(f'Elapsed time: {elapsed_time:0.2f}')
     return web.Response(text=json.dumps(results))
 
-# The RUN Level
+
 app = web.Application()
 app.add_routes([
     web.get('/', main),
 ])
+
 
 if __name__ == '__main__':
     web.run_app(app)
