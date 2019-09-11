c49cccbf17f7580a1e4391d292afbbd687992d26
Skyler Burger <skylerburger@gmail.com>
2019-09-11 10:00:50 -0700

    Add new files

---
+++ app/__main__.py
@@ -0,0 +1,23 @@
+from aiohttp import web, ClientSession
+
+import asyncio
+
+from . import api_fetch
+
+def main():
+    
+
+    async def start_async_app():
+        runner = web.AppRunner(github)
+        await runner.setup()
+        
+        return runner
+
+    loop = asyncio.get_event_loop()
+    runner = loop.run_until_complete(start_async_app())
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.run_until_complete(runner.cleanup())
+
+main()
